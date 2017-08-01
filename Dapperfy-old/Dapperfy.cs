64be16643f5f75374669217df13d1de2cf01797d
Brandon W. Gandy <brandongundy@gmail.com>
2017-07-31 19:27:33 -0500

    rename old dapperfy project

--- Dapperfy-old/Dapperfy.cs
+++ Dapperfy-old/Dapperfy.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 
-namespace DapperApiTest
+namespace Dapperfy
 {
     public static class Dapperfy
     {
@@ -15,17 +15,15 @@ namespace DapperApiTest
             return GenerateFindByIdSelectQuery(id, tableName, pkName);
         }
 
-
         /// <summary>
         /// Generates a SELECT query that selects all records with a table name of obj with an "s" at the end.
         /// For example, if you have a Person class, this method assumes the table name is Persons.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="obj"></param>
         /// <returns></returns>
-        public static string GetAll<T>(T obj) where T : Type
+        public static string GetAll<T>() where T : class
         {
-            return GenerateStarSelectQuery($"{obj.Name}s");
+            return GenerateStarSelectQuery($"{typeof(T).GetType()}s");
         }
 
         /// <summary>
