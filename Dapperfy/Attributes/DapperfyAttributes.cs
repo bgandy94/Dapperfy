05fe1c79f823d22842dccef880e1ffc3eafc5868
Brandon W. Gandy <brandongundy@gmail.com>
2017-07-31 19:36:49 -0500

    add new project and move over files

---
+++ Dapperfy/Attributes/DapperfyAttributes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dapperfy.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class PrimaryKeyAttribute : Attribute
+    {
+
+    }
+
+
+}
